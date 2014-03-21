ffba8f1ebc286b9f7e4d061eeab61c78931ccf5f
Alexander Riccio <alexander@riccio.com>
2014-03-21 16:42:25 -0400

    obsessive-compulsive formatting

--- SetBrightCSharp/ConsoleApplication3/ConsoleApplication3/Program.cs
+++ SetBrightCSharp/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -9,50 +9,41 @@ using System.Windows.Forms;
 namespace DisplayBrightnessConsole {
 
     public static class BaseConst {
-        //BaseConst( ) { }
-        public const double Version = 0.3;
         static public int GetHourBright( int position ) {
             return HourBright[ position ];
             }
-        private static readonly int[] HourBright = { 22, 44, 54, 76, 83, 92, 97, 100 };
+        static public double GetVersion( ) {
+            return Version;
+            }
+        private static readonly int[]  HourBright = { 22, 44, 54, 76, 83, 92, 97, 100 };
+        private const double Version    = 0.3;
         }
-
-
 
     internal class Program {
 
         private static int GetBrightness( ) {
-        System.Management.ManagementScope               s   = null;
-        System.Management.SelectQuery                   q   = null;
-        System.Management.ManagementObjectSearcher      mos = null;
-        System.Management.ManagementObjectCollection    moc = null;
-        int curBrightness = 0;
-            try {
-                //System.Management.ManagementScope               s   =     new System.Management.ManagementScope( "root\\WMI" );
-                //System.Management.SelectQuery                   q   =     new System.Management.SelectQuery( "WmiMonitorBrightness" );
+            System.Management.ManagementScope               s   = null;
+            System.Management.SelectQuery                   q   = null;
+            System.Management.ManagementObjectSearcher      mos = null;
+            System.Management.ManagementObjectCollection    moc = null;
+            int curBrightness = 0;
                 
+            try {
                 s   =     new System.Management.ManagementScope( "root\\WMI" );
-            
                 q   =     new System.Management.SelectQuery( "WmiMonitorBrightness" );
-
                 mos =     new System.Management.ManagementObjectSearcher( s, q );
-
                 moc =     mos.Get( );
 
                 foreach ( System.Management.ManagementObject o in moc ) {
-
                     curBrightness = Convert.ToInt32( Convert.ToString( o.GetPropertyValue( "CurrentBrightness" ) ) );
-
                     break; //only work on the first object
-
                     }
-            }
-        finally {
-            moc.Dispose( );
-            mos.Dispose( );
-            }
-            return curBrightness;
-            
+                }
+            finally {
+                moc.Dispose( );
+                mos.Dispose( );
+                }
+                return curBrightness;
             }
 
         private static int[ ] GetBrightnessLevels( ) {
@@ -61,36 +52,26 @@ namespace DisplayBrightnessConsole {
             System.Management.SelectQuery                   q   = null;
             System.Management.ManagementObjectSearcher      mos = null;
             System.Management.ManagementObjectCollection    moc = null;
+
             int[] retBrightnessLevels = null;
             try {
-                s = new System.Management.ManagementScope( "root\\WMI" );
-
-                q = new System.Management.SelectQuery( "WmiMonitorBrightness" );
-
+                s   = new System.Management.ManagementScope( "root\\WMI" );
+                q   = new System.Management.SelectQuery( "WmiMonitorBrightness" );
                 mos = new System.Management.ManagementObjectSearcher( s, q );
-
                 moc = mos.Get( );
-
-
 
                 byte[] BrightnessLevels = new byte[ 0 ];//store result
 
                 foreach ( System.Management.ManagementObject o in moc ) {
-
                     Console.Write( " " + o + "; " );
-
                     BrightnessLevels = ( byte[ ] )o.GetPropertyValue( "Level" );
-
                     break; //only work on the first object
-
                     }
-
+                
                 retBrightnessLevels = new int[ BrightnessLevels.Length ];
-
+                
                 for ( int i  = 0; i < BrightnessLevels.Length; i++ ) {
-
                     retBrightnessLevels[ i ] = Convert.ToInt32( BrightnessLevels[ i ] );
-
                     }
                 }
             finally {
@@ -102,41 +83,40 @@ namespace DisplayBrightnessConsole {
 
         
         private static void displayWMIInfo( ) {
-
             System.Management.ManagementScope               s   = null;
             System.Management.SelectQuery                   q   = null;
             System.Management.ManagementObjectSearcher      mos = null;
             System.Management.ManagementObjectCollection    moc = null;
+            
             int[] BrightnessLevels = null;
+
             try {
-                s = new System.Management.ManagementScope( "root\\WMI" );
-
-                q = new System.Management.SelectQuery( "WmiMonitorBrightness" );
-
+                s   = new System.Management.ManagementScope( "root\\WMI" );
+                q   = new System.Management.SelectQuery( "WmiMonitorBrightness" );
                 mos = new System.Management.ManagementObjectSearcher( s, q );
-
                 moc = mos.Get( );
-
 
                 BrightnessLevels = new int[ 0 ];//store result
 
                 foreach ( System.Management.ManagementObject o in moc ) {
-                    Console.WriteLine( o.ClassPath );
-                    Console.WriteLine( o.Container );
-                    Console.WriteLine( o.Options );
-                    Console.WriteLine( o.Path );
-                    Console.WriteLine( o.Properties );
-                    Console.WriteLine( o.Qualifiers );
-                    Console.WriteLine( o.Scope );
-                    Console.WriteLine( o.Site );
+                    Console.WriteLine( o.ClassPath        );
+                    Console.WriteLine( o.Container        );
+                    Console.WriteLine( o.Options          );
+                    Console.WriteLine( o.Path             );
+                    Console.WriteLine( o.Properties       );
+                    Console.WriteLine( o.Qualifiers       );
+                    Console.WriteLine( o.Scope            );
+                    Console.WriteLine( o.Site             );
                     Console.WriteLine( o.SystemProperties );
 
                     List<string> newget = new List<string>( );
-                    newget.Add( o.GetText( System.Management.TextFormat.Mof ) );
+                    
+                    newget.Add( o.GetText( System.Management.TextFormat.Mof      ) );
                     newget.Add( o.GetText( System.Management.TextFormat.CimDtd20 ) );
                     newget.Add( o.GetText( System.Management.TextFormat.WmiDtd20 ) );
 
                     string newget3 = o.GetText( System.Management.TextFormat.WmiDtd20 );
+                    
                     string[] newgetwords3 = Regex.Split( newget3, ">" );
                     string[][] lesstrings = new string[ 1 ][ ] { newgetwords3 };
 
@@ -149,10 +129,10 @@ namespace DisplayBrightnessConsole {
                             Console.Write( n );
                             }
                         }
-                    Console.WriteLine( o.GetPropertyQualifierValue( "Active", "CIMTYPE" ) );
+                    Console.WriteLine( o.GetPropertyQualifierValue( "Active",       "CIMTYPE" ) );
                     Console.WriteLine( o.GetPropertyQualifierValue( "InstanceName", "CIMTYPE" ) );
-                    Console.WriteLine( o.GetPropertyValue( "__RELPATH" ) );
-                    Console.WriteLine( o.GetPropertyValue( "__PATH" ) );
+                    Console.WriteLine( o.GetPropertyValue         ( "__RELPATH"               ) );
+                    Console.WriteLine( o.GetPropertyValue         ( "__PATH"                  ) );
                     }
                 }
             finally {
@@ -163,20 +143,22 @@ namespace DisplayBrightnessConsole {
 
         [STAThread]
         private static void Main( string[ ] args ) {
-            Console.WriteLine( "version " + BaseConst.Version + "\n" );
+            Console.WriteLine( "version " + BaseConst.GetVersion() + "\n" );
             NotifyIcon tray = null;
             try {
-                tray = new NotifyIcon( );
-                tray.Icon = SystemIcons.Application;
+                tray         = new NotifyIcon( );
+                tray.Icon    = SystemIcons.Application;
                 tray.Visible = true;
 
                 #region noargs
 
                 if ( args.Length == 0 ) {
-                    tray.ShowBalloonTip( 1000, "SetBright? SleepTight!", "version " + BaseConst.Version + " works, yo", ToolTipIcon.Info );
+                    tray.ShowBalloonTip( 1000, "SetBright? SleepTight!", "version " + BaseConst.GetVersion() + " works, yo", ToolTipIcon.Info );
                     Console.WriteLine( "DEBUG: Current brightness: " + GetBrightness( ) );
+                    
                     int hour = DateTime.Now.Hour;
-                    int min = DateTime.Now.Minute;
+                    int min  = DateTime.Now.Minute;
+                    
                     mapBright( hour, min );
                     System.Threading.Thread.Sleep( 1000 );
                     }
@@ -188,9 +170,11 @@ namespace DisplayBrightnessConsole {
                 else if ( args[ 0 ] == "-getlevels" ) {
                     int[] BrightnessLevels = GetBrightnessLevels( );
                     Console.Write( "This monitor supports " + ( BrightnessLevels.Length ) + " brightness levels: " );
+                    
                     foreach ( int b in BrightnessLevels ) {
                         Console.Write( b.ToString( ) + ", " );
                         }
+                    
                     }
 
                 #endregion getlevels
@@ -204,11 +188,10 @@ namespace DisplayBrightnessConsole {
                 else if ( args[ 0 ] == "-loop" )//test sequence
                 {
                     int[] BrightnessLevels = GetBrightnessLevels( );
-                    int initbright = GetBrightness( );
+                    int   initbright       = GetBrightness( );
 
                     foreach ( int b in BrightnessLevels ) {
                         SetBrightness( b );
-
                         Console.WriteLine( "Brightness->" + b.ToString( ) );
                         System.Threading.Thread.Sleep( 100 );
                         }
@@ -228,7 +211,7 @@ namespace DisplayBrightnessConsole {
 
                 else if ( args[ 0 ] == "-time" ) {
                     int hour = DateTime.Now.Hour;
-                    int min = DateTime.Now.Minute;
+                    int min  = DateTime.Now.Minute;
                     Console.WriteLine( "Current time is: " + hour + ":" + min );
                     }
 
@@ -239,22 +222,22 @@ namespace DisplayBrightnessConsole {
                     int targetBrightness;
                     bool res = int.TryParse( args[ 0 ], out targetBrightness );
 
-                    //parse switch value
                     if ( res == false ) {
                         Console.WriteLine( "Invalid input!, please try again" );
-                        } if ( res == true ) {
+                        return;
+                        } 
+                    if ( res == true ) {
                         if ( targetBrightness > 100 || targetBrightness < 0 ) //handles the wtf case where brightness is more than 100 or less than zero
-                        {
+                            {
                             Console.WriteLine( "What the hell?? Target brightness \"" + targetBrightness + "\" out of bounds!" );
+                        
                             int i = ( targetBrightness > 100 ? 100 : 0 );
-
-                            //byte l = ( ( byte )i );//required explicit type conversion
                             SetBrightness( i );
+                            
                             Console.WriteLine( "Brightness set to: \"" + i + "\" instead!" );
                             return;
                             }
 
-                        //byte h = ( ( byte )targetBrightness );//required explicit type conversion
                         SetBrightness( targetBrightness );
                         Console.WriteLine( "Current Brightness: " + GetBrightness( ) );
                         }
@@ -272,7 +255,7 @@ namespace DisplayBrightnessConsole {
                 }
             switch ( hourNow ) {
                 case 8:
-                    SetBrightness( BaseConst.GetHourBright( ( ( minuteNow < 30 ) ? 0 : 1 ) ) );//if not yet 8:30, 22; else 44
+                    SetBrightness( BaseConst.GetHourBright(     ( ( minuteNow < 30 ) ? 0 : 1 ) ) );//if not yet 8:30, 22; else 44
                     break;
 
                 case 9:
@@ -306,52 +289,40 @@ namespace DisplayBrightnessConsole {
             }
 
         private static void SetBrightness( int targetBrightness ) {
-        NotifyIcon tray = null;
-        System.Management.ManagementScope               s   = null;
-        System.Management.SelectQuery                   q   = null;
-        System.Management.ManagementObjectSearcher      mos = null;
-        System.Management.ManagementObjectCollection    moc = null;
+            NotifyIcon tray = null;
+            System.Management.ManagementScope               s   = null;
+            System.Management.SelectQuery                   q   = null;
+            System.Management.ManagementObjectSearcher      mos = null;
+            System.Management.ManagementObjectCollection    moc = null;
 
-        try {
-            Console.WriteLine( "Setting Brightness: " + targetBrightness );
+            try {
+                Console.WriteLine( "Setting Brightness: " + targetBrightness );
 
-            tray = new NotifyIcon( );
+                tray         = new NotifyIcon( );
+                tray.Icon    = SystemIcons.Application;
+                tray.Visible = true;
 
-            tray.Icon = SystemIcons.Application;
+                String targetBrightnessStr = targetBrightness.ToString( );
 
-            tray.Visible = true;
+                tray.ShowBalloonTip( 1000, "Setting Brightness: ", targetBrightnessStr, ToolTipIcon.Info );
 
-            String targetBrightnessStr = targetBrightness.ToString( );
+                s   =     new System.Management.ManagementScope( "root\\WMI" );
+                q   =     new System.Management.SelectQuery( "WmiMonitorBrightnessMethods" );
+                mos =     new System.Management.ManagementObjectSearcher( s, q );
+                moc =     mos.Get( );
 
-            tray.ShowBalloonTip( 1000, "Setting Brightness: ", targetBrightnessStr, ToolTipIcon.Info );
-
-            s   =     new System.Management.ManagementScope( "root\\WMI" );
-
-            q   =     new System.Management.SelectQuery( "WmiMonitorBrightnessMethods" );
-
-            mos =     new System.Management.ManagementObjectSearcher( s, q );
-
-            moc =     mos.Get( );
-
-            
-
-            foreach ( System.Management.ManagementObject o in moc )//output current brightness
-            
-                {
-
-                o.InvokeMethod( "WmiSetBrightness", new Object[ ] { UInt32.MaxValue, targetBrightness } ); //note the reversed order - won't work otherwise!
-
-                break; //only work on the first object
+                foreach ( System.Management.ManagementObject o in moc )//output current brightness
+                    {
+                    o.InvokeMethod( "WmiSetBrightness", new Object[ ] { UInt32.MaxValue, targetBrightness } ); //note the reversed order - won't work otherwise!
+                    break; //only work on the first object
+                    }
 
                 }
-
-            
-            }
-        finally {
-            moc.Dispose( );
-            mos.Dispose( );
-            tray.Dispose( );
-            }
+            finally {
+                moc.Dispose( );
+                mos.Dispose( );
+                tray.Dispose( );
+                }
             }
         }
     }
